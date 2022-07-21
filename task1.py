1b8cfb5fc90c70e41f9e8e641d4d3c30287823e7
Islombek Mukhtorov <tanlanganasarlar@gmail.com>
2022-07-21 12:39:07 +0300

    updated code

--- task1.py
+++ task1.py
@@ -1,15 +1,17 @@
 # #https://leetcode.com/problems/palindrome-number/
 class Solution(object):
     def isPalindrome(self, x):
-        if x < 0:
-            return False
-        else:
-            reverse_x = ''
-            for i in range(-1,-len(str(x))-1,-1):
-                reverse_x += str(x)[i]
-            if x == int(reverse_x):
-                return True
-            return False
-                
+        # if x < 0:
+        #     return False
+        # else:
+        #     reverse_x = ''
+        #     for i in range(-1,-len(str(x))-1,-1):
+        #         reverse_x += str(x)[i]
+        #     if x == int(reverse_x):
+        #         return True
+        #     return False
+        reversedd = str(x)[::-1]
+        resp = True if reversedd == str(x) else False
+        return resp
 e1 = Solution()
-print(e1.isPalindrome(151))
+print(e1.isPalindrome(152))
